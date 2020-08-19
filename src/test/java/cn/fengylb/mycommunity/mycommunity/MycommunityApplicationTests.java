770be932e4b0b78fba1eea86693b72b96f5a8fce
fengyunlong <fyl19920820@163.com>
2020-08-19 22:42:56 +0800

    add first

---
+++ src/test/java/cn/fengylb/mycommunity/mycommunity/MycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fengylb.mycommunity.mycommunity;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
