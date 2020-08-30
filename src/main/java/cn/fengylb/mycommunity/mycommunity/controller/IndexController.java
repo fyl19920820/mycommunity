e0a71ef0006886257e476b0586721ed42dfa3f73
fengyunlong <fyl19920820@163.com>
2020-08-30 20:18:34 +0800

    增加拦截器和资源映射器，资源映射器解决js和css加载错误的问题

--- src/main/java/cn/fengylb/mycommunity/mycommunity/controller/IndexController.java
+++ src/main/java/cn/fengylb/mycommunity/mycommunity/controller/IndexController.java
@@ -1,8 +1,6 @@
 package cn.fengylb.mycommunity.mycommunity.controller;
 
 import cn.fengylb.mycommunity.mycommunity.dto.PaginationDTO;
-import cn.fengylb.mycommunity.mycommunity.dto.User;
-import cn.fengylb.mycommunity.mycommunity.mapper.UserMapper;
 import cn.fengylb.mycommunity.mycommunity.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,29 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
     @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private QuestionService questionService;
     @GetMapping("/")
     public String index(HttpServletRequest request, Model model, @RequestParam(value = "page",defaultValue = "1")Integer page, @RequestParam(value = "size",defaultValue = "5")Integer size){
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length > 0){
-            for (Cookie cookie : cookies){
-                if (cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                }
-            }
-        }
         PaginationDTO paginationDTO = questionService.list(page,size);
         model.addAttribute("pagination",paginationDTO);
         return "index";
--- src/main/java/cn/fengylb/mycommunity/mycommunity/controller/ProfileController.java
+++ src/main/java/cn/fengylb/mycommunity/mycommunity/controller/ProfileController.java
@@ -2,7 +2,6 @@ package cn.fengylb.mycommunity.mycommunity.controller;
 
 import cn.fengylb.mycommunity.mycommunity.dto.PaginationDTO;
 import cn.fengylb.mycommunity.mycommunity.dto.User;
-import cn.fengylb.mycommunity.mycommunity.mapper.UserMapper;
 import cn.fengylb.mycommunity.mycommunity.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,13 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-    @Autowired
-    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
     @GetMapping("/profile/{section}")
@@ -25,19 +21,7 @@ public class ProfileController {
                           @RequestParam(value = "page",defaultValue = "1")Integer page,
                           @RequestParam(value = "size",defaultValue = "5")Integer size,
                           @PathVariable(name = "section")String section){
-        Cookie[] cookies = request.getCookies();
-        User user = null;
-        if (cookies != null && cookies.length > 0){
-            for (Cookie cookie : cookies){
-                if (cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                }
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null){
             return "redirect:/";
         }
