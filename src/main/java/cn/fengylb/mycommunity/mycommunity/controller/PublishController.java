eff4a86c766fd0676ee3bc5c2d5aafa84cfac4d1
fengyunlong <fyl19920820@163.com>
2020-08-31 22:43:51 +0800

    编辑功能

--- src/main/java/cn/fengylb/mycommunity/mycommunity/controller/PublishController.java
+++ src/main/java/cn/fengylb/mycommunity/mycommunity/controller/PublishController.java
@@ -1,28 +1,28 @@
 package cn.fengylb.mycommunity.mycommunity.controller;
 
 import cn.fengylb.mycommunity.mycommunity.dto.Question;
+import cn.fengylb.mycommunity.mycommunity.dto.QuestionDTO;
 import cn.fengylb.mycommunity.mycommunity.dto.User;
 import cn.fengylb.mycommunity.mycommunity.mapper.QuestionMapper;
 import cn.fengylb.mycommunity.mycommunity.mapper.UserMapper;
+import cn.fengylb.mycommunity.mycommunity.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
-    private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
+    private QuestionService questionService;
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish() {
         return "publish";
     }
 
@@ -31,40 +31,51 @@ public class PublishController {
             @RequestParam(value = "title", required = false) String title,
             @RequestParam(value = "description", required = false) String description,
             @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Long id,
             HttpServletRequest request,
             Model model) {
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
-        if (title == null || title.trim().equals("")){
-            model.addAttribute("error","标题不能为空");
-            return "publish";
-        };
-        if (description == null || description.trim().equals("")){
-            model.addAttribute("error","描述不能为空");
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        if (title == null || title.trim().equals("")) {
+            model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if (tag == null || tag.trim().equals("")){
-            model.addAttribute("error","标签不能为空");
+        ;
+        if (description == null || description.trim().equals("")) {
+            model.addAttribute("error", "描述不能为空");
+            return "publish";
+        }
+        if (tag == null || tag.trim().equals("")) {
+            model.addAttribute("error", "标签不能为空");
             return "publish";
         }
 
         User user = (User) request.getSession().getAttribute("user");
-        if (user == null){
-            model.addAttribute("error","用户未登录");
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
             return "publish";
         }
 
         Question question = new Question();
+        question.setId(id);
         question.setTitle(title);
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(Long.valueOf(user.getId()));
         question.setAccountId(user.getAccountId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+        questionService.createOrUpdate(question);
         return "redirect:/";
 
+    }
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable("id")Long id,Model model){
+        QuestionDTO questionDTO = questionService.findById(id);
+        model.addAttribute("title", questionDTO.getTitle());
+        model.addAttribute("description", questionDTO.getDescription());
+        model.addAttribute("tag", questionDTO.getTag());
+        model.addAttribute("id",questionDTO.getId());
+        return "publish";
     }
 }
