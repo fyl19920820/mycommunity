c3f7cc6a25947f4d14f68986c1cfa5690e74968a
fengyunlong <fyl19920820@163.com>
2020-09-19 10:39:01 +0800

    修复通知功能

--- src/main/java/cn/fengylb/mycommunity/mycommunity/interceptor/SessionInterceptor.java
+++ src/main/java/cn/fengylb/mycommunity/mycommunity/interceptor/SessionInterceptor.java
@@ -3,6 +3,7 @@ package cn.fengylb.mycommunity.mycommunity.interceptor;
 import cn.fengylb.mycommunity.mycommunity.dto.User;
 import cn.fengylb.mycommunity.mycommunity.dto.UserExample;
 import cn.fengylb.mycommunity.mycommunity.mapper.UserMapper;
+import cn.fengylb.mycommunity.mycommunity.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,6 +18,8 @@ import java.util.List;
 public class SessionInterceptor implements HandlerInterceptor {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private NotificationService notificationService;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
@@ -29,6 +32,8 @@ public class SessionInterceptor implements HandlerInterceptor {
                     List<User> users = userMapper.selectByExample(userExample);
                     if (users != null && users.size() == 1){
                         request.getSession().setAttribute("user",users.get(0));
+                        Long unreadCount = notificationService.unreadCount(users.get(0).getId());
+                        request.getSession().setAttribute("unreadCount", unreadCount);
                     }
                 }
             }
